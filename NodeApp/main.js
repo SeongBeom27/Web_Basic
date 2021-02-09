3f7e40179645866b74517a926d5db9998b1b6fb7
SeongBeom27 <lgh121546@naver.com>
2021-02-09 16:44:36 +0900

    Update App to a function type

--- NodeApp/main.js
+++ NodeApp/main.js
@@ -1,42 +1,56 @@
 var http = require('http');
 var fs = require('fs');
 var url = require('url');
+const { Http2ServerRequest } = require('http2');
 
+function templateHTML(title, list, body) {
+    return `
+    <!doctype html>
+    <html>
+    <head>
+    <title>WEB1 - ${title}</title>
+    <meta charset="utf-8">
+    </head>
+    <body>
+    <h1><a href="/">WEB</a></h1>
+    ${list}
+    ${body}
+    </body>
+    </html>
+    `;
+}
+
+function templateList(filelist) {
+    var list = '<ol>';
+    var i = 0;
+    while (i < filelist.length) {
+        list += `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`;
+        i = i + 1;
+    }
+    list = list + '</ol>';
+    return list;
+}
 var app = http.createServer(function(request, response) {
     var _url = request.url;
     var queryData = url.parse(_url, true).query;
     var pathname = url.parse(_url, true).pathname;
-    var title = queryData.id
 
     // pathname으로는 home과 다른 페이지를 구별할 수 없다.
     if (pathname == '/') {
-        fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description) {
-            if (queryData.id == undefined) {
-                title = 'Welcome';
-                description = 'Hello, Node.js';
-            }
-            var template = `
-            <!doctype html>
-            <html>
-            <head>
-            <title>WEB1 - ${title}</title>
-            <meta charset="utf-8">
-            </head>
-            <body>
-            <h1><a href="/">WEB</a></h1>
-            <ol>
-                <li><a href="/?id=HTML">HTML</a></li>
-                <li><a href="/?id=CSS">CSS</a></li>
-                <li><a href="/?id=Javascript">JavaScript</a></li>
-            </ol>
-            <h2>${title}</h2>
-            <p>${description}</p>
-            </body>
-            </html>
-            `
+        fs.readdir('./data', function(err, filelist) {
+            fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description) {
+                var list = templateList(filelist);
+                var title = queryData.id;
+                // homepage의 경우
+                if (queryData.id == undefined) {
+                    title = 'Welcome';
+                    description = 'Hello, Node.js';
+                }
+                var template = templateHTML(title, list, `<h2>${title}</h2>${description}`);
+                response.writeHead(200);
                 // 사람들이 실제로 받게되는 화면 data를 reponse.end(data)로 넘겨준다.
-            response.writeHead(200);
-            response.end(template);
+                response.end(template);
+            });
         });
     } else {
         response.writeHead(404);
